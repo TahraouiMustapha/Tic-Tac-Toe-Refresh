a0d4718191955de8eb71d4b69a9312b018bdbbf5
mustapha <moustaphaoussama1818@gmail.com>
2024-11-15 09:55:08 +0100

    Feat : Create a gameboard object, game inside an IIFE and Create the factory createPlayer

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('hi')
+const gameboard = (function () {
+    gameboardArray = [];
+
+    const getGameboard = () => gameboardArray ;
+
+    const fillGameboard = (choice, marker) => {
+        if( !gameboardArray[choice] ) {
+            gameboardArray[choice] = marker;
+        }
+    }
+
+    return {
+        getGameboard,
+        fillGameboard
+    }
+
+})();
+
+function createPlayer( name, marker) {
+    return {
+        name, 
+        marker
+    }
+}
+
+const game = (function() {
+    const player1 = createPlayer('mohamed', 'X');
+    const player2 = createPlayer('Abdo', 'O');
+    
+
+
+    const startGame = () => {
+        console.log('start game');
+        gameboard.fillGameboard(0, player1.marker);
+        gameboard.fillGameboard(1, player2.marker);
+    
+        console.log( gameboard.getGameboard() )
+    }
+
+    return {
+        startGame
+    }
+    
+})();
+
+
+game.startGame();
